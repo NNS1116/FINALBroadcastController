a470f820130009b7b4076ee3ad777c6f91b28d63
Nahlan <nahlan-nahla@outlook.com>
2024-10-10 00:15:31 -0400

    Add project files.

---
+++ FINALBroadcastController/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BroadcastLibrary;
+
+namespace BroadcastAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BroadcastController : ControllerBase
+    {
+        private readonly Broadcast _broadcast;
+
+        public BroadcastController()
+        {
+            _broadcast = new Broadcast();
+        }
+
+        [HttpGet("getBroadcastMessage")]
+        public IActionResult GetBroadcastMessage()
+        {
+            var message = _broadcast.BroadcastMessage();
+            return Ok(message);
+        }
+    }
+}
